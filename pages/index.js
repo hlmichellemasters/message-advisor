82ef67963d095234820f28359c7aa3f027a531ed
hlmichellemasters <hlmichellemasters@gmail.com>
2023-05-07 12:09:38 -0500

    change animal names to message advice

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,33 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import Head from 'next/head';
+import { useState } from 'react';
+import styles from './index.module.css';
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [messageInput, setMessageInput] = useState('');
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
     try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
+      const response = await fetch('/api/generate-advice', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ message: messageInput }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+      setMessageInput('');
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
@@ -34,22 +37,22 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Message Advisor</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Get Advice on Messages</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="message"
+            placeholder="Enter a message"
+            value={messageInput}
+            onChange={(e) => setMessageInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Get Advice" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
